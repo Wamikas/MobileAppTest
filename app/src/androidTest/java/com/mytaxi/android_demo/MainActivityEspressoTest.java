c8fd9f1613b72011a8c10844ec076bfaacbedcc3
Wamikas <wamikas@ItsMe-Wamika.local>
2018-08-09 12:58:23 +0200

    Append permissions

--- app/src/androidTest/java/com/mytaxi/android_demo/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MainActivityEspressoTest.java
@@ -1,5 +1,6 @@
 package com.mytaxi.android_demo;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.mytaxi.android_demo.activities.MainActivity;
@@ -17,7 +18,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
@@ -29,6 +29,10 @@ public class MainActivityEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule =
+            GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
 
     @Before
