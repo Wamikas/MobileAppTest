61bd11786ffc4c8ea4f641ca300ad2ae569e301f
Wamikas <wamikas@ItsMe-Wamika.local>
2018-08-09 10:45:23 +0200

    Fixing the search drop down selection

--- app/src/androidTest/java/com/mytaxi/android_demo/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MainActivityEspressoTest.java
@@ -14,10 +14,14 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
@@ -25,31 +29,30 @@ public class MainActivityEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
-    private MainActivity mActivity = mActivityRule.getActivity();
 
 
     @Before
-    public void login() throws InterruptedException {
+    public void login() {
+
         String username="crazydog335";
         String password="venture";
         onView(withId(R.id.edt_username)).perform(typeText(username), closeSoftKeyboard());
         onView(withId(R.id.edt_password)).perform(typeText(password), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
-    }
 
-    //TODO: remove async waits
+    }
 
     @Test
     public void testSearchAndCall() throws InterruptedException {
         String driverName = "Sarah Scott";
-        Thread.sleep(3000);
-        onView(withId(R.id.searchContainer)).check(matches(isDisplayed()));
-        onView(withId(R.id.textSearch)).perform(typeText("sa"), closeSoftKeyboard());
-        Thread.sleep(3000);
-        onView(withId(R.id.drawer_layout)).perform(open());
-        onView(withText("Logout")).perform(click());
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+        onView(withId(R.id.textSearch)).perform(typeText("sa"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withText(driverName)).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
+       .perform(click());
         onView(withId(R.id.fab)).perform(click());
+//        onView(withId(R.id.drawer_layout)).perform(open());
+//        onView(withText("Logout")).perform(click());
+
     }
 
 }
